ccf8bd9c4cdc366f387936226e219defecae7837
lvlvipv2 <120413351+lvlvipv2@users.noreply.github.com>
2022-12-12 21:31:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 if (!Akashi.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
 if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 m = smsg(Akashi, mek, store)
-require("./Core")(Akashi, m, chatUpdate, store)
+require("./Akashi")(Akashi, m, chatUpdate, store)
 } catch (err) {
 console.log(err)
 }
